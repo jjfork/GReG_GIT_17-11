a6d48b936e7be26109db39b5f298e76bc1183a92
jjfork <jakubiakrjulian@gmail.com>
2023-11-24 01:03:33 +0100

    test

--- calculator.py
+++ calculator.py
@@ -1,26 +1,24 @@
-def add(x, y):
-    return x + y
+def add(x: int, y: int) -> int:
+   return x + y
 
+def subtract(x: int, y: int) -> int:
+   return x - y
 
-def subtract(x, y):
-    pass
+def divide(x: float, y: float) -> float:
+   if y == 0:
+       raise ValueError("Division by zero is not allowed.")
+   return x / y
 
+def multiply(x: int, y: int) -> int:
+   return x * y
 
-def divide(x, y):
-    pass
+def square(x: int) -> int:
+   return x ** 2
 
+def power(x: float, y: float) -> float:
+   return x ** y
 
-def multiply(x, y) -> int:
-    pass
-
-
-def square(x):
-    pass
-
-
-def power(x, y):
-    pass
-
-
-def sqrt(x):
-    pass
+def sqrt(x: float) -> float:
+   if x < 0:
+       raise ValueError("Cannot compute the square root of a negative number.")
+   return x ** 0.5
